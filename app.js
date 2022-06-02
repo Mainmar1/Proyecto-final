a7bbcdf47c7e1ab23b64d79a5c591290e3b63b13
Mainmar1 <juan_bj95@hotmail.com>
2022-06-02 00:54:39 -0300

    new

--- app.js
+++ app.js
@@ -71,11 +71,16 @@ items.innerHTML = ''
 
     const template = document.querySelector('#template-carrito').content
     const fragment = document.createDocumentFragment()
+
 Object.values(carrito).forEach(producto =>{
     template.querySelector('th').textContent = producto.id
     template.querySelectorAll('td')[0].textContent = producto.title
     template.querySelectorAll('td')[1].textContent = producto.cantidad
     template.querySelector('span').textContent = producto.precio * producto.cantidad
+
+    template.querySelector('.btn-info').dataset.id = producto.id
+    template.querySelector('.btn-danger').dataset.id = producto.id
+    
 
     const clone = template.cloneNode(true)
     fragment.appendChild(clone)
@@ -83,8 +88,82 @@ Object.values(carrito).forEach(producto =>{
 
 items.appendChild(fragment)
 
+pintarFooter()
+accionBotones()
+
 }
 
+const footer = document.querySelector('#footer-carrito')
+const pintarFooter = () =>{
+
+footer.innerHTML = ''
+
+if(Object.keys(carrito).length === 0){
+
+    footer.innerHTML = `<th scope="row" colspan="5">Carrito vacío!</th>`
+
+    return
+
+}
+
+const template = document.querySelector('#template-footer').content
+const fragment = document.createDocumentFragment()
+
+const nCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad, 0)
+const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio, 0)
+
+template.querySelectorAll('td')[0].textContent = nCantidad
+template.querySelector('span').textContent = nPrecio
+
+
+
+const clone = template.cloneNode(true)
+
+fragment.appendChild(clone)
+
+footer.appendChild(fragment)
+
+
+const boton = document.querySelector('#vaciar-carrito')
+boton.addEventListener('click', ()=>{
+    carrito = {}
+    pintarCarrito()
+})
+}
+
+const accionBotones = () =>{
+
+const botonesAgregar = document.querySelectorAll('#items .btn-info')
+const botonesEliminar = document.querySelectorAll('#items .btn-danger')
+
+botonesAgregar.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+const producto = carrito[btn.dataset.id]
+producto.cantidad ++
+
+carrito[btn.dataset.id] = {...producto}
+pintarCarrito()
+
+    })
+})
+
+botonesEliminar.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        
+        const producto = carrito[btn.dataset.id]
+        producto.cantidad--
+        if(producto.cantidad === 0){
+
+          delete carrito[btn.dataset.id]
+
+        }else {
+            carrito[btn.dataset.id] = {...producto}
+        }
+        pintarCarrito()
+    })
+})
+    
+}
 
 
 
