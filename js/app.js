78bc6b21081bdcd2cab45256338d95250565538d
Mainmar1 <juan_bj95@hotmail.com>
2022-07-10 17:19:44 -0300

    new push

---
+++ js/app.js
@@ -0,0 +1,211 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetchData()
+})
+
+const fetchData = async () => {
+    try{
+const res = await fetch('api.json')
+const data = await res.json()
+
+// console.log(data)
+
+pintarProductos(data)
+detectarBotones(data)
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+
+// PRODUCTOS EN HTML CON UN FOREACH
+
+const contenedor_productos = document.querySelector('#contenedor_productos')
+
+const pintarProductos = (data) => {
+
+    const template = document.querySelector('#template_productos').content
+    const fragment = document.createDocumentFragment()
+   
+    data.forEach(producto =>{
+
+        // console.log(producto)
+
+        template.querySelector('img').setAttribute('src', producto.thumbnailUrl)
+        template.querySelector('h5').textContent = producto.title
+        template.querySelector('p span').textContent = producto.precio
+        template.querySelector('button').dataset.id = producto.id
+
+        const clone = template.cloneNode(true)
+        fragment.appendChild(clone)
+
+    })
+
+    contenedor_productos.appendChild(fragment)
+}
+
+let carrito = {}
+ 
+
+const detectarBotones = (data) => {
+const botones = document.querySelectorAll('.card button')
+botones.forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+        const producto = data.find(item => item.id === parseInt(btn.dataset.id))
+        producto.cantidad = 1
+        if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad +1
+        }
+
+        carrito[producto.id] = {...producto}
+        pintarCarrito()
+    })
+})
+// console.log(botones)
+}
+
+const items = document.querySelector('#items')
+
+const pintarCarrito = () =>{
+
+items.innerHTML = ''
+
+    const template = document.querySelector('#template-carrito').content
+    const fragment = document.createDocumentFragment()
+
+Object.values(carrito).forEach(producto =>{
+    template.querySelector('th').textContent = producto.id
+    template.querySelectorAll('td')[0].textContent = producto.title
+    template.querySelectorAll('td')[1].textContent = producto.cantidad
+    template.querySelector('span').textContent = producto.precio * producto.cantidad
+
+    template.querySelector('.btn-info').dataset.id = producto.id
+    template.querySelector('.btn-danger').dataset.id = producto.id
+    
+
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+})
+
+items.appendChild(fragment)
+
+pintarFooter()
+accionBotones()
+
+}
+
+const footer = document.querySelector('#footer-carrito')
+const pintarFooter = () =>{
+
+footer.innerHTML = ''
+
+if(Object.keys(carrito).length === 0){
+
+    footer.innerHTML = `<th scope="row" colspan="5">Carrito vacío!</th>`
+
+    return
+
+}
+
+const template = document.querySelector('#template-footer').content
+const fragment = document.createDocumentFragment()
+
+
+const nCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad, 0)
+const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio, 0)
+
+template.querySelectorAll('td')[0].textContent = nCantidad
+template.querySelector('span').textContent = nPrecio
+
+
+
+const clone = template.cloneNode(true)
+
+fragment.appendChild(clone)
+
+footer.appendChild(fragment)
+
+
+const boton = document.querySelector('#vaciar-carrito')
+boton.addEventListener('click', ()=>{
+    carrito = {}
+    pintarCarrito()
+})
+}
+
+const accionBotones = () =>{
+
+const botonesAgregar = document.querySelectorAll('#items .btn-info')
+const botonesEliminar = document.querySelectorAll('#items .btn-danger')
+
+botonesAgregar.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+const producto = carrito[btn.dataset.id]
+producto.cantidad ++
+
+carrito[btn.dataset.id] = {...producto}
+pintarCarrito()
+
+    })
+})
+
+botonesEliminar.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        
+        const producto = carrito[btn.dataset.id]
+        producto.cantidad--
+        if(producto.cantidad === 0){
+
+          delete carrito[btn.dataset.id]
+
+        }else {
+            carrito[btn.dataset.id] = {...producto}
+        }
+        pintarCarrito()
+    })
+})
+    
+}
+
+
+document.getElementById("btn_cerrar_sesion").addEventListener('click', ()=>{
+
+    // let rta = confirm("Desea cerrar sesion?");
+
+
+    // if(rta){
+    //     window.location.href = "./index.html";
+    //     swal("¡La sesion se cerrro con exito!","","success");
+    // }
+
+    swal({
+        title: "Esas seguro que deseas cerrar sesion?",
+        text: "Si cierras sesion se perdera lo almacenado en el carrito",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("La cuenta se cerro con exito!", {
+            icon: "success",
+            
+          });
+          window.location.href = "./index.html";
+        } else {
+          swal("A seguir comprando!");
+        }
+      });
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/app1.js
@@ -0,0 +1,89 @@
+const $btnSignIn = document.querySelector(".sign_in_btn"),
+  $btnSignUp = document.querySelector(".sign_up_btn"),
+  $signUp = document.querySelector(".sign-up"),
+  $signIn = document.querySelector(".sign-in");
+
+document.addEventListener("click", (e) => {
+  if (e.target === $btnSignIn || e.target === $btnSignUp) {
+    $signUp.classList.toggle("active");
+    $signIn.classList.toggle("active");
+  }
+});
+
+let ingreso = 0;
+
+document.getElementById("btnRegistrarse").addEventListener("click", (e) => {
+  const name_usuario = document.getElementById("name_usuario").value;
+  const email_usuario = document.getElementById("email_usuario").value;
+  const password_usuario = document.getElementById("password_usuario").value;
+
+  const storageParsed =
+    JSON.parse(localStorage.getItem("registeredUsers")) ?? [];
+
+  const validationEmailRegistered = storageParsed.filter(
+    (element) => element.email === email_usuario
+  );
+
+  if (validationEmailRegistered.length > 0) {
+    swal(
+      `Please enter another email address, ${email_usuario} already in use`,
+      "",
+      "warning"
+    );
+    return;
+  }
+
+  if (name_usuario === "" || email_usuario === "" || password_usuario === "") {
+    swal("Todos los campos de registro deben estar completos", "", "warning");
+    return;
+  } else {
+    swal("Good Job!", "The account was successfully created", "success");
+    $signUp.classList.toggle("active");
+    $signIn.classList.toggle("active");
+  }
+
+  const newRegisteredUserObject = {
+    username: name_usuario,
+    email: email_usuario,
+    password: password_usuario,
+  };
+
+  storageParsed.push(newRegisteredUserObject);
+
+  localStorage.setItem("registeredUsers", JSON.stringify(storageParsed));
+});
+
+// INICIO DE SESION
+
+document.getElementById("btnInicionSesion").addEventListener("click", (e) => {
+  let ingreso_email = document.getElementById("validacion_email").value;
+  let ingreso_password = document.getElementById("validacion_password").value;
+
+  if (ingreso_email === "" || ingreso_password === "") {
+    swal("Todos los campos de ingreso deben estar completos", "", "warning");
+    return;
+  }
+
+  const storageParsed =
+    JSON.parse(localStorage.getItem("registeredUsers")) ?? false;
+
+  if (!storageParsed) {
+    swal("Not registered users");
+    return;
+  }
+
+  storageParsed.forEach((element) => {
+    const emailValidated =
+      element.email.toUpperCase() === ingreso_email.toUpperCase() ?? false;
+    const passwordValidated =
+      element.password.toUpperCase() === ingreso_password.toUpperCase() ??
+      false;
+
+    if (emailValidated && passwordValidated) {
+      swal("¡Access Successfully", " ", "success");
+      window.location.href = "./carritoCompras.html";
+    } else if (!emailValidated || !passwordValidated) {
+      swal("Email or password incorrect", "", "error");
+    }
+  });
+});
